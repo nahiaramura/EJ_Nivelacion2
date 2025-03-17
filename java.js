0bfad492575764c25972a85eff4e82b4766b385b
nahiaramura <48178605@est.ort.edu.ar>
2025-03-17 09:03:49 -0300

    logica termianda

---
+++ java.js
@@ -0,0 +1,31 @@
+let random = Math.floor(Math.random() * 100) + 1;  
+let contadorIntentos = 0;
+
+function adivinarNumRnd(event) {
+    event.preventDefault(); 
+   
+    let numero = parseInt(document.getElementById('numero').value);
+
+
+    if (isNaN(numero) || numero < 1 || numero > 100) {
+        alert("Por favor, ingrese un número válido entre 1 y 100.");
+        return false; 
+    }
+
+    contadorIntentos++;
+
+    if (numero < random) {
+        alert("Su número es menor al número generado. Intente nuevamente.");
+    } else if (numero > random) {
+        alert("Su número es mayor al número generado. Intente nuevamente.");
+    } else {
+        alert("¡Felicitaciones! Adivinaste el número en " + contadorIntentos + " intentos.");
+        return true;  
+    }
+
+    document.getElementById('numero').value = '';
+
+
+    return false;
+}
+
